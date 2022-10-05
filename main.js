febff44a0ffc4f7e6bcd6cd129dda9dac18bb408
umurung <101242087+umurung@users.noreply.github.com>
2022-10-05 12:05:36 +0200

    summary

---
+++ main.js
@@ -0,0 +1,7 @@
+
+import {printName as printUserName, printAge } from '/user.js'
+
+const user = new U('Bob', 11)
+console.log(user)
+printUserName(user)
+printAge(user)
---
+++ user.js
@@ -0,0 +1,16 @@
+export default class User {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+export function printName(user) {
+    console.log(`User's name is ${user.name}`)
+}
+
+export function printAge(user) {
+    console.log(`User is ${user.age} years old`)
+}
+
+
+
